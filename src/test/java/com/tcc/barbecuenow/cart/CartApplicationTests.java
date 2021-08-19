3bfb558fb562577821ebfd2531aed372badd7939
gabriel <gabriel.talles00@gmail.com>
2021-08-19 18:32:30 -0300

    fisrt commit

---
+++ src/test/java/com/tcc/barbecuenow/cart/CartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.barbecuenow.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
